8c2eefa66c71bc65cd34e38a4814fafe139e6c1f
angercc <87264662+angercc@users.noreply.github.com>
2022-11-24 19:36:45 +0000

    Create ThankYou.lua

---
+++ ThankYou.lua
@@ -0,0 +1,5 @@
+game.StarterGui:SetCore("SendNotification", {
+                                                                                                                    Title = "Horizon",
+                                                                                                                    Text = "Thank you for using Horizon!",
+                                                                                                                    Duration = 5
+                                                                                                                })
